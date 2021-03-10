6cde11162305f6ed5523f2aa42ede4dcbd0555dd
Maximiliano <maximilianoalbeldas@yahoo.com.ar>
2021-03-10 17:32:12 -0300

    Agregamos la configuracion al servidor

--- database/config/config.js
+++ database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "malbeldas",
+    "password": "Maradona_11",
+    "database": "malbeldas_misnotitas",
+    "host": "mysql-malbeldas.alwaysdata.net",
     "dialect": "mysql"
   }
 }
